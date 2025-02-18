332aa7b7b3cac261de99988e5b0865e24301c4aa
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-18 14:37:09 +0530

    add static file serving and catch-all route in server.js

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ import { dirname,join } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use(express.static(join(__dirname,'dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(join(__dirname,'dist','index.html'));
+});
+
