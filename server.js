dfd8b73cedcd2a6ff9f3de2f15fb408871e8ce4f
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-18 14:38:46 +0530

    configure Socket.io server with CORS settings

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
 import { dirname,join } from 'path';
+import { create } from 'domain';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,4 +15,12 @@ app.use(express.static(join(__dirname,'dist')));
 app.get('*',(req,res)=>{
     res.sendFile(join(__dirname,'dist','index.html'));
 });
+
+const server= createServer(app);
+const io = new Server(server,{
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+})
 
